352657244f38245f73c278fa529453fd2c427018
Ryan Ilano <ryan.ilano.690@my.csun.edu>
2022-10-10 08:29:10 -0700

    Prettier - enforce double quotes

--- utils/verification.ts
+++ utils/verification.ts
@@ -1,7 +1,7 @@
 exports.generateVerificationCall = () => {
   //Module generates a random four digit number
   // TODO: Temporary, for prod. utilze an API to generate randm tokens.
-  let verificationCall = '';
+  let verificationCall = "";
   for (let i = 0; i <= 3; i += 1) {
     const randVal = Math.round(Math.random() * 9);
     verificationCall += randVal;
